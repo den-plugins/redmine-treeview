69b9474f0b8b956bc2c99fe0f0bbc918c8d6b8ad
lvillasica <lvillasica@lvillasica.(none)>
2012-03-01 13:58:56 +0800

    Split feature > keep ids of trasferred subtasks.

--- assets/javascripts/split.js
+++ assets/javascripts/split.js
@@ -3,6 +3,7 @@ function transfer_task(id, type){
   var subtask = jQuery("#s_" + id);
   jQuery("#no_tasks_" + type).remove();
   jQuery("#transfer_table_" + type).append("<tr>" + subtask.html() + "</tr>").find(".small").remove();
+  jQuery("#transferred_" + type).val(jQuery("#transferred_" + type).val() + id + " ");
   subtask.hide();
 
 }
