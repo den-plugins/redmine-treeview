5f71f8484baa5e3d16931e097e662110d844f974
Stefany Serino <stefanyserino@gmail.com>
2012-03-26 11:19:39 +0800

    apply issues/show changes to user stories

--- app/controllers/treeview_controller.rb
+++ app/controllers/treeview_controller.rb
@@ -64,9 +64,19 @@ class TreeviewController < IssuesController
   end
   
   def show
-    @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
-    @journals.each_with_index {|j,i| j.indice = i+1}
-    @journals.reverse! if User.current.wants_comments_in_reverse_order?
+    @total_journals = @issue.journals.count("journals.id", :include => [:user, :details],
+                                      :conditions => ["#{JournalDetail.table_name}.property <> 'timelog' OR #{Journal.table_name}.notes is not null AND #{Journal.table_name}.notes <> ''"])
+    @journal_pages = IssuesController::Paginator.new self, @total_journals, 10, params['page']
+    offset = @journal_pages.current.offset
+    @journals = @issue.journals.find(:all, :include => [:user, :details], :limit => 10, :offset => offset,
+                                     :conditions => ["#{JournalDetail.table_name}.property <> 'timelog' OR #{Journal.table_name}.notes is not null AND #{Journal.table_name}.notes <> ''"],
+                                     :order => "#{Journal.table_name}.created_on #{User.current.wants_comments_in_reverse_order? ? 'DESC' : 'ASC'}")
+    
+    if User.current.wants_comments_in_reverse_order?
+      @journals.each_with_index {|j,i| j.indice = (@total_journals - offset) - i}
+    else
+      @journals.each_with_index {|j,i| j.indice = (offset + i)+1}
+    end
     @changesets = @issue.changesets
     @changesets.reverse! if User.current.wants_comments_in_reverse_order?
     @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
@@ -78,17 +88,16 @@ class TreeviewController < IssuesController
     @time_entry = TimeEntry.new
     @update_options = {'Internal (DEN only)' => 1, 'Include Mystic' => 2}
     @treeview_index = "#{root_url}stories/#{@project.identifier}"
-
-		# for feature #8364
-		issues = @project.issues.collect {|pi| pi.id}.sort
-		index = issues.index(@issue.id)
-		if !index.nil?
-			prv_issue = (index-1 >= 0) ? issues[index-1] : 0
-			nxt_issue = (index+1 < issues.size) ? issues[index+1] : 0
-		end
-		#
+    
+    # for feature #8364
+    issues = @project.issues.collect {|pi| pi.id}.sort
+    index = issues.index(@issue.id)
+    if !index.nil?
+      prv_issue = (index-1 >= 0) ? issues[index-1] : 0
+      nxt_issue = (index+1 < issues.size) ? issues[index+1] : 0
+    end
     respond_to do |format|
-      format.html { render :template => 'treeview/show.rhtml', :locals => {:prv => prv_issue, :nxt => nxt_issue} } # for feature #8364 - added locals
+      format.html { render :template => 'issues/show.rhtml', :locals => {:prv => prv_issue, :nxt => nxt_issue}, :layout => !request.xhr? } # for feature #8364 - added locals
       format.atom { render :action => 'changes', :layout => false, :content_type => 'application/atom+xml' }
       format.pdf  { send_data(issue_to_pdf(@issue), :type => 'application/pdf', :filename => "#{@project.identifier}-#{@issue.id}.pdf") }
     end
@@ -436,8 +445,6 @@ class TreeviewController < IssuesController
     
   private
   def treeview_authorize(action = params[:action])
-    puts action
-    puts @project
     allowed = User.current.allowed_to?({:controller => 'issues', :action => action}, @project)
     allowed ? true : deny_access
   end
