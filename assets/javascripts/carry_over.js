36754aecac3c2751f2c9d449f677affada24184d
Normz <johnnormancapule@gmail.com>
2012-07-03 14:08:53 +0800

    Added jQuery function for carry over.

---
+++ assets/javascripts/carry_over.js
@@ -0,0 +1,34 @@
+var j = jQuery.noConflict();
+
+var CarryOver = {
+  init: function() {
+    var me = this;
+    me.buttons();
+  },
+  
+  buttons: function() {
+    j(".transfer_button").live("click", function(){
+      if(j(this).hasClass("from")) {
+        var row = j(this).parents("tr")[0];
+        var table = j("#transfer_table_new");
+        j("#no_tasks_new").hide();
+        j(row).appendTo(table.find("tbody"));
+        j(this).removeClass("from").addClass("to");
+      }
+      else if(j(this).hasClass("to")) {
+        var row = j(this).parents("tr")[0];
+        var origin_table = j("#splittable_list");
+        var transfer_table = j("#transfer_table_new");
+        j(row).appendTo(origin_table.find("tbody"));
+        j(this).addClass("from").removeClass("to");
+        if(transfer_table.find("tbody").children().length==0)
+          j("#no_tasks_new").show();
+      }
+    });
+  }
+}
+
+j(document).ready(function(){
+  CarryOver.init();
+});
+
