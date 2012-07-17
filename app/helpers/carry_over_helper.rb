c933ced05a450f0ce94c78dd0ddf98e5a6a4e4db
Normz <johnnormancapule@gmail.com>
2012-07-17 12:46:23 +0800

    #60240(Merge Task to Parent's previously 'Carried Over' tree)

--- app/helpers/carry_over_helper.rb
+++ app/helpers/carry_over_helper.rb
@@ -4,19 +4,16 @@ module CarryOverHelper
     original = Issue.find params[:id]
     carry_over = params[:carry_over_to]
     carries = carry_over[:issues]
-    issue = original.custom_clone
-    issue.attributes = self.create_attributes(carry_over, issue)
+    issue, tree_list = self.tree_merge(self.create_attributes(carry_over, original))
     if carries and carries!=""
       begin
         issue.save!
       rescue ActiveRecord::RecordInvalid
         return false
       else
-        tree_list = [issue]
         carries = carries.split(",").map{|x| Issue.find(x.to_i)}
         arr = self.get_issues_to_be_carried(original.children, carries)
-        flag = arr.empty?
-        while !flag
+        while arr.count > 0
           arr.each do |c|
             if c.parent.issue_from_id != original.id
               arr << c.parent.issue_from
@@ -26,23 +23,50 @@ module CarryOverHelper
             end
             arr.delete c
           end
-          flag = arr.empty?
         end
       return true
       end
     end
   end
-
+  
+  # merge carry over to existing
+  def self.tree_merge(issue)
+    tree_list = []
+    exist_parent = Issue.find(:all, :conditions=>{:subject=>issue.subject,
+                                                  :fixed_version_id=>issue.fixed_version.id}).last
+    if exist_parent
+      issue = exist_parent
+      arr = issue.children
+      while arr.count > 0
+        arr.each do |i|
+          tree_list << i
+          arr += i.children
+          arr.delete i
+        end
+      end
+    end
+    [issue, tree_list << issue]
+  end
+  
+  def self.create_attributes(carry_over, original)
+    issue = original.custom_clone
+    hash = {"subject" => "#{carry_over[:subject]}[CO]",
+            "priority_id" => carry_over[:priority_id],
+            "description" => carry_over[:description],
+            "fixed_version_id" => carry_over[:fixed_version_id]}
+    hash["custom_field_values"] = issue.hashify_custom_values
+    issue.attributes = hash
+    issue
+  end
+  
   def self.get_issues_to_be_carried(arr, carries)
     ret = []
-    flag = arr.empty?
-    while !flag
+    while arr.count > 0
       arr.each do |x|
         arr.delete x
         arr += x.children
         ret << x if carries.include?(x)
       end
-      flag = arr.empty?
     end
     ret
   end
@@ -56,11 +80,11 @@ module CarryOverHelper
       new = issue.custom_clone
       new.subject = new.subject.gsub(/(\[CO\])+/, "") + "[CO]"
       new.attributes = {"fixed_version_id" => version.id,
-                        "custom_field_values" => self.create_custom_fields(issue.custom_values)}
+                        "custom_field_values" => issue.hashify_custom_values}
       if new.save
         tree_list << new
         issue = Issue.find(issue.id)
-        issue.status = IssueStatus.find_by_name("Carried Over")
+        #issue.status = IssueStatus.find_by_name("Carried Over")
         puts "issue #{issue.save}"
       end
     else
@@ -71,7 +95,7 @@ module CarryOverHelper
       np = parent.custom_clone
       np.subject = np.subject.gsub(/(\[CO\])+/, "") + "[CO]"
       np.attributes = {"fixed_version_id" => version.id,
-                       "custom_field_values" => self.create_custom_fields(parent.custom_values)}
+                       "custom_field_values" => parent.hashify_custom_values}
       if np.save
         tree_list << np
         parent = Issue.find(parent.id)
@@ -97,34 +121,17 @@ module CarryOverHelper
     tree_list
   end
   
-  def self.create_attributes(carry_over, issue)
-    hash = {"subject" => "#{carry_over[:subject]}[CO]",
-            "priority_id" => carry_over[:priority_id],
-            "description" => carry_over[:description],
-            "fixed_version_id" => carry_over[:fixed_version_id]}
-    hash["custom_field_values"] = self.create_custom_fields(issue.custom_values) if !issue.custom_values.empty?
-    hash
-  end
-
-  def self.create_custom_fields(custom_values)
-    hash = {}
-    custom_values.each do |c|
-      hash["#{c.custom_field_id}"] = c.value
-    end
-    hash
-  end
-
+  #get leaf nodes of the tree
   def self.get_children(parent)
-    flag = false
     arr = parent.children
     children = []
-    while !flag
+    while arr.count > 0
       arr.each do |x|
         arr.delete x
         x.children.empty? ? children << x : arr += x.children
       end
-      flag = arr.empty?
     end
     children
   end
+
 end
