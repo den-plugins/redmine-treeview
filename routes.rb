f33d0ee7ef59850b65e13a26ebc41917431d19d3
Normz <johnnormancapule@gmail.com>
2012-07-23 16:39:29 +0800

    Updated routes

--- routes.rb
+++ routes.rb
@@ -1,11 +1,13 @@
-map.with_options :controller => 'treeview' do |group_routes|
-  group_routes.with_options :conditions => {:method => :get} do |group_views|
-    group_views.connect 'stories/:project_id', :action => 'index'
-    group_views.connect 'treeview/:action', :action => 'context_menu'
-  end
-  group_routes.with_options :conditions => {:method => :post} do |group_actions|
-    group_actions.connect 'stories/:project_id', :action => 'index'
-    group_actions.connect 'treeview/:action', :action => 'context_menu'
-    group_actions.connect 'treeview/create_iteration'
+ActionController::Routing::Routes.draw do |map|
+  map.with_options :controller => 'treeview' do |group_routes|
+    group_routes.with_options :conditions => {:method => :get} do |group_views|
+      group_views.connect 'stories/:project_id', :action => 'index'
+      group_views.connect 'treeview/:action', :action => 'context_menu'
+    end
+    group_routes.with_options :conditions => {:method => :post} do |group_actions|
+      group_actions.connect 'stories/:project_id', :action => 'index'
+      group_actions.connect 'treeview/:action', :action => 'context_menu'
+      group_actions.connect 'treeview/create_iteration'
+    end
   end
 end
