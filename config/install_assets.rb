f091026b740185f684ff9c8104cb6d1d510dc3b3
Normz <johnnormancapule@gmail.com>
2012-07-27 11:52:25 +0800

    Updated asset installation

--- config/install_assets.rb
+++ config/install_assets.rb
@@ -1,17 +1,11 @@
 root     = RAILS_ROOT
 curr_dir = File.dirname(__FILE__)
 
-js_dest    = %Q{#{root}/public/javascripts/redmine_treeview}
-js_orig    = %Q{#{curr_dir}/../assets/javascripts/}
-style_dest = %Q{#{root}/public/stylesheets/redmine_treeview}
-style_orig = %Q{#{curr_dir}/../assets/stylesheets/}
+asset_dest    = %Q{#{root}/public/plugin_assets/redmine_treeview}
+asset_orig    = %Q{#{curr_dir}/../assets/}
 
 #clean all installed assets
-FileUtils.rm_r js_dest, :force => true
-FileUtils.rm_r style_dest, :force => true
+FileUtils.rm_r asset_dest, :force => true
 
-#copy all js assets to <app>/public/javascripts/redmine_treeview
-FileUtils.cp_r js_orig, js_dest
-
-#copy all stylesheet assets to <app>/public/stylesheets/redmine_treeview
-FileUtils.cp_r style_orig, style_dest
+#copy all js assets to <app>/public/plugin_assets/redmine_treeview
+FileUtils.cp_r asset_orig, asset_dest
